18f9fb5823db859d888c45796f1ebf06acdb7456
Parish Gole <parishgole.workmail@gmail.com>
2024-01-06 10:32:01 +0530

    java file added

---
+++ Assmt72.java
@@ -0,0 +1,23 @@
+package XpathAssmts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assmt72 {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+ChromeDriver driver=new ChromeDriver();
+		
+		driver.get("https://www.amazon.in");
+		driver.manage().window().maximize();
+	    Thread.sleep(2000);
+	    
+	    driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("shoes");
+	    driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys(Keys.ENTER);
+	   
+
+	}
+
+}
